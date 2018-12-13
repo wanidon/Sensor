32abae96897bf890a175590b57264bfb0878030a
wanidon <wii2datalost@gmail.com>
2018-12-13 10:25:38 +0900

    made Cameratest.py

--- seven-segment/Cameratest.py
+++ seven-segment/Cameratest.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python2
 #coding:utf-8
+import time
+import picamera
+import commands
+#from PIL import Image, ImageOps
+#import ToBin
+
+def cameraTest():
+        with  picamera.PiCamera() as cam:
+                cam.resolution = (400,300)
+                cam.start_preview()
+                time.sleep(8)
+                cam.capture("testimage.jpg")
+
+if __name__ == "__main__":
+    while True:
+        cameraTest()
+
+        
--- seven-segment/ConfigApp.py
+++ seven-segment/ConfigApp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 #coding:utf-8
 from Tkinter import *
 from ttk import *
